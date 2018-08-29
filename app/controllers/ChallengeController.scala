e6cd38242253140ff5d65ac86f70588d9a75d834
Samael Alejandro Zapata Ibarra - Ceiba Software <alejandro.zapata@ceiba.com.co>
2018-08-29 15:26:09 -0500

    Retos completos hasta la sección Routing

--- app/controllers/ChallengeController.scala
+++ app/controllers/ChallengeController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject.Inject
 import org.apache.commons.lang3.Validate
-import play.api.mvc.{AbstractController, ControllerComponents, PlayBodyParsers}
+import play.api.mvc.{AbstractController, ControllerComponents, Cookie, PlayBodyParsers}
 
 import scala.concurrent.ExecutionContext
 
@@ -27,6 +27,32 @@ class ChallengeController @Inject()(cc: ControllerComponents, parser: PlayBodyPa
   def putRequest = Action { request => Ok("Put request!") }
   def deleteRequest = Action { request => Ok("Delete request!") }
   def patchRequest = Action { request => Ok("Patch request!") }
+
+  def sendParameter(parameter: String) = Action { request =>
+    Ok(s"Hello, the parameter: $parameter has been received!")
+  }
+
+  def requestWithHeader() = Action {
+    request => {
+      val headers = request.headers;
+      val login = headers.get("Login").get;
+      Ok(s"Request received for login: $login")
+    }
+  }
+
+  def requestWithCookies() = Action { request =>
+    val someOrg = request.cookies.get("org")
+    val org = someOrg match {
+      case Some(n) => n
+      case None => "Without cookies"
+    }
+    /*Si quiero mostrar solo el value de la cookie
+      val orgValue = org.toString.split(",")
+      Ok(s"Received the cookie: ${orgValue(1)}")*/
+      Ok(s"Received the cookie: $org")
+      .withCookies(Cookie("org", "Ceiba"))
+      .bakeCookies()
+  }
 
 
 
